b6abcf7520b7b4defef55024ca574717e14012cd
BadMagic100 <dempsey.sean@outlook.com>
2026-01-01 22:57:18 -0800

    Set print width to 100 (#2)

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,10 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
 const config = {
   proseWrap: "always",
+  printWidth: 100,
 };
 
 export default config;
